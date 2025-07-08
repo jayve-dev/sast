9ee003d55c70c7253d4a6de159c8af7a4cf41582
jayve-dev <monleonjayvee7@gmail.com>
2025-07-08 11:13:54 +0800

    dashboard added some contents

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,10 +1,21 @@
 "use client"
 import React from 'react'
+import { Button } from './ui/button'
 
 const SideBar = () => {
   return (
-    <nav>
-    </nav>
+    <div className='w-auto h-full max-h-[500px] bg-[#f79b72] p-7 rounded-lg shadow-lg shadow-[#0A0A0A]/50'>
+      <div className='w-full h-full flex flex-col items-center justify-center gap-4'>
+        <Button>Home</Button>
+        <Button>Home</Button>
+        <Button>Home</Button>
+        <Button>Home</Button>
+        <Button>Home</Button>
+        <Button>Home</Button>
+      </div>
+    
+      
+    </div>
   )
 }
 
