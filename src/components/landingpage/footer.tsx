2870f1e9951dd6819421f66696c838596c388526
zrlldy <jeroldcurry@gmail.com>
2025-08-05 16:17:24 +0800

    chore: final design for  the ladning page

---
+++ src/components/landingpage/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function footer() {
+return (
+<div>
+    <footer className="w-full bg-black text-white py-2 fixed bottom-0 z-50">
+        <div className='flex text-sm font-semibold text-white items-center justify-center'> @2025 Copyright: SAST</div>
+
+    </footer>
+</div>
+);
+}
+
+export default footer;
